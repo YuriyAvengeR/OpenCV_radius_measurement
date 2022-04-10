401e5e0a7a74af211c234bcaf0b1a188cc68095e
YuriyAvengeR <32493905+YuriyAvengeR@users.noreply.github.com>
2022-04-10 16:33:12 +0300

    Add files via upload

--- _openCV.py
+++ _openCV.py
@@ -2,33 +2,48 @@ import cv2
 import numpy as np
 import random
 from decorators import size_checker, text_size
-
+from connect import db_connect, target_color, target_name, tolerance_measurement
+from math import sqrt
 
 class Output:
 
     @staticmethod
     def frame():
+        cap = cv2.VideoCapture(0)
         while True:
-            #image = cv2.imread('C:\\Users\\yura0\\Desktop\\5.jpg')
+
+            # Select method video or image
+            #ret, image = cap.read()
             image = cv2.imread('C:\\Users\\yura0\\Desktop\\1_1.jpg')
-            #image = cv2.imread('C:\\Users\\yura0\\Desktop\\measure_object_size\\phone_aruco_marker.jpg')
 
-            # Get Aruco marker
-            parameters = cv2.aruco.DetectorParameters_create()
-            aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
-            corners, _, _ = cv2.aruco.detectMarkers(image, aruco_dict, parameters = parameters)
+            try:
+                # Aruco setup
+                parameters = cv2.aruco.DetectorParameters_create()
+                aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_5X5_50)
+                corners, _, _ = cv2.aruco.detectMarkers(image, aruco_dict, parameters = parameters)
 
-            # Draw polygon around the marker
-            int_corners = np.int0(corners)
-            cv2.polylines(image, int_corners, True, (0, 255, 0), 1)
+                # Aruco draw border
+                int_corners = np.int0(corners)
+                #cv2.polylines(image, int_corners, True, (0, 255, 0), 1)
 
-            # Aruco Perimeter
-            aruco_perimeter = cv2.arcLength(corners[0], True)
+                # Aruco get perimeter
+                aruco_perimeter = cv2.arcLength(corners[0], True)
 
-            # Pixel to cm ratio
+                # Text Label for Aruco status
+                cv2.putText(image, "ACF", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (107,255,53), 3)
+            except(IndexError):
+                aruco_perimeter = 1
+                cv2.putText(image, "ANF", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (53,255,236), 3)
+
+            # Pixel graduation
             pixel_cm_ratio = aruco_perimeter / 20
 
-            # Scale
+            # Get origin aruco size on cm
+            aruco_size = (((aruco_perimeter / pixel_cm_ratio) / 4) /2.1)
+            aruco_size = (aruco_size * sqrt(2)) / 2
+            aruco_size = round(aruco_size, 2)
+
+            # Picture scaling
             scale_percent = 100 # scale count
             width = int(image.shape[1] * scale_percent / 100)
             height = int(image.shape[0] * scale_percent / 100)
@@ -40,22 +55,24 @@ class Output:
             blur = cv2.GaussianBlur(gray, (3,3), 0)
             thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
-            # Morph open with a elliptical shaped kernel
+            # Morph
             kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
             opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
 
-            # Find contours and draw minimum enclosing circle
+            radius_list = []
+            data = 0
+
+            # Find contours [main]
             cnts = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             cnts = cnts[0] if len(cnts) == 2 else cnts[1]
             for c in cnts:
+
                 # Get Width and Height of the Objects by applying the Ratio pixel to cm
                 (x, y), r = cv2.minEnclosingCircle(c)
                 r = round(r, 2)
 
                 # Convert px to radius in cm
-                object_radius = round(((r / pixel_cm_ratio) / 2.1), 3)
-                print(object_radius)
-
+                object_radius = round(((r / pixel_cm_ratio) / 2.1), 2)
                 cv2.putText(image,
                             f"R{str(object_radius)} cm | {random.randint(1, 5)}",
                             (int(int(size_checker(int(y), int(r), int(x))[1])),
@@ -63,15 +80,39 @@ class Output:
                             cv2.FONT_HERSHEY_SIMPLEX,
                             text_size(r)[0], (0, 0, 255),
                             text_size(r)[1])
+
+                # Select object to detect
+                try:
+                    object_radius = round(object_radius, 2)
+                    radius_list.append(object_radius)
+                    list_len = len(radius_list)
+                    if list_len == 2:
+                        radius_list.remove(aruco_size)
+                        data = float(radius_list[0])
+                except(ValueError):
+                    radius_list.clear()
+
+                # Init current data about object
+                obj_size = db_connect()[1]
+                obj_tol = db_connect()[2]
+
                 # Main stroke
-                #cv2.circle(image,(int(x), int(y)), int(r), (0,0,255), 2)
+                cv2.circle(image,(int(x), int(y)), int(r), (0,0,255), 2)
                 # Center point
                 cv2.circle(image, (int(x), int(y)), 2, (0, 0, 255), 2)
                 # Note line
                 cv2.line(image, (int(x), int(y)), (int(x) + int(r), int(y)), (0, 0, 255), 2)
+                # Target status | note found
+                cv2.putText(image, target_name(), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (target_color()), 3)
+                # Target size | cm
+                cv2.putText(image, f"{obj_size}", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (target_color()), 3)
+                # Target tolerance | cm
+                cv2.putText(image, f"{obj_tol}", (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 1, (target_color()), 3)
+                # Target measurement STATUS
+                cv2.putText(image, f"{tolerance_measurement(data)[0]}", (10, dim[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, tolerance_measurement(data)[1], 3)
 
             cv2.imshow('output post processing', image)
-            cv2.imshow("thresh", thresh)
+            #cv2.imshow("thresh", thresh)
 
             k = cv2.waitKey(10) & 0xff
             if k == 27:
---
+++ connect.py
@@ -0,0 +1,98 @@
+import random
+import sqlite3
+import datetime
+import time
+
+
+def db_connect():
+    sqlite_file = 'C:\\Users\\yura0\\Desktop\\measurement_system\\ObjectDB.db'
+    db = sqlite3.connect(sqlite_file)
+    sql = db.cursor()
+    sql.execute(f"SELECT Name, Size, Tol, Ind FROM current WHERE status = {int(25)}")
+    data = sql.fetchone()
+    db.close()
+    current_name = data[0]
+    current_size = data[1]
+    current_tol = data[2]
+    current_index = data[3]
+    return current_name, current_size, current_tol, current_index
+
+def target_color():
+    index = db_connect()[3]
+    if index is 0:
+        yellow_color = 53, 255, 236
+        return yellow_color
+    if index is 1:
+        green_color = 107,255,53
+        return green_color
+    else:
+        error_color = 0, 0, 255
+        return error_color
+
+
+def target_name():
+    index = db_connect()[3]
+    if index is 0:
+        status_name_not_found = "TNF"
+        return status_name_not_found
+    if index is 1:
+        status_name_found = "TCF"
+        return status_name_found
+    else:
+        return "Error"
+
+def zero_init():
+    sqlite_file = 'C:\\Users\\yura0\\Desktop\\measurement_system\\ObjectDB.db'
+    db = sqlite3.connect(sqlite_file)
+    sql = db.cursor()
+    sql.execute(f'UPDATE current SET Ind = "0" WHERE status = {int(25)}')
+    db.commit()
+    db.close()
+
+
+def tolerance_measurement(measure_size):
+    index = db_connect()[3]
+    if index is 1:
+        origin_name = db_connect()[0]
+        origin_size = db_connect()[1]
+        origin_tol = db_connect()[2]
+        if float(measure_size) >= float(origin_size) + float(origin_tol) or float(measure_size) <= float(origin_size) - float(origin_tol):
+            status_time = datetime.datetime.now()
+            status_color_red = 75, 63, 255
+            text_status = "STATUS"
+            log_text = f"{origin_name} A DEFECT AT {status_time}"
+            return text_status, status_color_red, log_text, 0
+        elif float(measure_size) <= float(measure_size) + float(origin_tol) or float(measure_size) >= float(measure_size) - float(origin_tol):
+            status_time = datetime.datetime.now()
+            status_color_green = 107,255,53
+            text_status = "STATUS"
+            return text_status, status_color_green, status_time, 1
+    if index is 0:
+        status_time = datetime.datetime.now()
+        status_yellow_color = 53, 255, 236
+        text_status = "RULLER MODE"
+        return text_status, status_yellow_color, status_time, 2
+    else:
+        return "error"
+
+
+# True:
+    #print(tolerance_measurement(1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
